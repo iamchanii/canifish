b2260697c95a0b79576d65f5de7a947050a3ed9d
iamchanii <iamchanii.0310@gmail.com>
2020-03-26 21:16:19 +0900

    :wrench: Add jest configurations

---
+++ packages/database/src/__tests__/parseSpreadsheet.test.ts
@@ -0,0 +1,8 @@
+import { parsePlaceText } from '../parseSpreadsheet';
+
+describe('Test', () => {
+  it('should fail', () => {
+    parsePlaceText('test');
+    fail('Not implements yet');
+  });
+});
