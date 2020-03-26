5578a58ee350c45cd67218957ee9a91d68d127b2
iamchanii <iamchanii.0310@gmail.com>
2020-03-26 20:12:48 +0900

    :art: Update prettier configuartion and reformat all codes

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { fishDatabase } from "@canifish/database";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { fishDatabase } from '@canifish/database';
 
 function App() {
   console.log(fishDatabase);
