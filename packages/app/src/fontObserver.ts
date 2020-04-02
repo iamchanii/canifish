60c07b52f2e2eaa3582ba06c2b9626428d0850b2
iamchanii <iamchanii.0310@gmail.com>
2020-04-02 16:44:12 +0900

    :lipstick: Update font face observer options

--- packages/app/src/fontObserver.ts
+++ packages/app/src/fontObserver.ts
@@ -1,6 +1,8 @@
 import FontFaceObserver from 'fontfaceobserver';
 
-const font = new FontFaceObserver('NanumSquareRound');
+const font = new FontFaceObserver('NanumSquareRound', {
+  weight: 800,
+});
 
 font.load().then(() => {
   document.body.classList.add('font-loaded');
